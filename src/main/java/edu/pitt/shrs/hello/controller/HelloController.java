f328fac821f026bd1d7e87a300e35d0285f19619
Gede Pramana <wira.yudha@gmail.com>
2020-04-28 17:55:31 -0400

    Initial commit

---
+++ src/main/java/edu/pitt/shrs/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package edu.pitt.shrs.hello.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
